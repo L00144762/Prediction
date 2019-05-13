893a5447093d84c10aa61cfc91a571ec1f015ada
Curran Richard <L00144762@lyitnet.lyit.ie>
2019-05-13 12:01:06 +0100

    initial

---
+++ LinearRegression.R
@@ -0,0 +1,20 @@
+# example uses "women" dataset
+# that contains height and weight of 15 women aged 30 - 39
+
+str(women)
+
+
+simple_linear_model <- lm(weight ~ height, data = women)
+simple_linear_model
+# shows the intercept and beta coefficient for height variable
+# ie weight = -87.52 + 3.45 x height
+
+plot(women$height,
+     women$weight, 
+     xlab = "Height (inches)",
+     ylab = "Weight (lbs)",
+     main = "Scatter plot showing regression line for weight
+     predicted from height")
+
+
+abline(simple_linear_model)
